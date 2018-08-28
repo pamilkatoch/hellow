64cb05f19f108d234f1f8ffb5f26cf6e59b22d76
pamilkatoch <42728579+pamilkatoch@users.noreply.github.com>
2018-08-28 10:10:41 +0530

    Add files via upload

---
+++ assgt.2/2.java
@@ -0,0 +1,7 @@
+class q
+{
+public static void main(String args[])
+{
+System.out.printf(""+args[0]);
+}
+} 
---
+++ assgt.2/2a.java
@@ -0,0 +1,7 @@
+class asd
+{
+public static void main(String args[])
+{
+System.out.printf("Hello World");
+}
+} 
